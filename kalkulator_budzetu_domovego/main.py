ecd89dab6a76642b9e299d6c70e2f97c816d7420
Oleksii Iordan <X-5096@DC=sntis,DC=local>
2023-12-14 15:45:13 +0100

    Changes to budget calculator

--- kalkulator_budzetu_domovego/main.py
+++ kalkulator_budzetu_domovego/main.py
@@ -6,36 +6,50 @@ class Budget():
 
 
     def add_expence_income(self, status, value):
-        self.transactions[status] = value
+        self.transactions.append(status)
+        self.transactions.append(value)
         self.save_file()
         print("Expense or income was added")
 
 
     def save_file(self):
         with open(self.filename, 'w') as file:
-            for status, value in self.transactions.items():
-                file.write(f"{status}/ {value}")
+            i=0
+            while i < len(self.transactions):
+                file.write(f"{self.transactions[i]}")
+                i+=2
         print("File saved")
 
 
     def read_file(self):
-        transactions = {}
+        transactions = []
         try:
             with open(self.filename, 'r') as file:
                 for line in file:
                     status, value = line.strip().split('/ ')
-                    transactions[status] = value
+                    transactions.append(status)
+                    print(status)
+                    transactions.append(value)
+                    print(value)
         except FileNotFoundError:
             pass
         return transactions
 
 
     def summary(self):
-        pass
+        revenue = 0
+        expenses = 0
 
 
     def display_transactions(self):
-        pass
+        i=0
+        while i < len(self.transactions):
+            print(self.transactions[i], end= '')
+            i+=1
+            print(self.transactions[i])
+            i+=1
+
+        
 
 
 
@@ -45,11 +59,12 @@ while True:
     choise = input("Enter your choise:\n1. Add an expense/income\n2. Generate a summary of expenses and revenue\n3. View transaction history\n4. Quit\n")
     if choise == '1':
         status = input("Enter the status of the transaction(+/-): ")
-        value = input("Enter a value of ")
+        value = input("Enter a value of the transaction")
+        budget.add_expence_income(status, value)
     elif choise == '2':
         pass
     elif choise == '3':
-        pass
+        budget.display_transactions()
     elif choise == '4':
         break
     else:
